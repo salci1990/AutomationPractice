b0d997da1dbb099ecc65a3d5c60b4351b01a044c
Artek <a.kowalski@ariesrk.pl>
2019-02-15 13:25:33 +0100

    added new @Test totalPriceTest

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,12 +1,13 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import page_objects.Header;
-import page_objects.SignInPage;
+import page_objects.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +17,9 @@ public class MainApp {
     private WebDriverWait wait;
     private SignInPage signInPage;
     private Header header;
+    private IndexPage indexPage;
+    private LayerCard layerCard;
+    private ShoppingCard shoppingCard;
 
     @BeforeClass
     public void setUp() {
@@ -28,6 +32,11 @@ public class MainApp {
 
         signInPage = new SignInPage(driver);
         header = new Header(driver);
+        indexPage = new IndexPage(driver);
+        signInPage = new SignInPage(driver);
+        header = new Header(driver);
+        layerCard = new LayerCard(driver);
+        shoppingCard = new ShoppingCard(driver);
     }
 
     @AfterClass
@@ -41,6 +50,41 @@ public class MainApp {
         header.clickLoginLink();
         signInPage.clickSignInLink();
         assertThat(signInPage.isAlertOn());
+    }
+
+    @Test
+    public void TotalPriceTest() {
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+        Actions builder = new Actions(driver);
+        builder.moveToElement(indexPage.findElement().get(2)).build().perform();
+
+        indexPage.clickAddButton();
+
+        wait.until(ExpectedConditions.visibilityOf(layerCard.exitWindow()));
+        assertThat(layerCard.exitWindow().isDisplayed()).isTrue();
+        layerCard.exitWindowSuccess();
+
+        builder.moveToElement(indexPage.findElement().get(3)).build().perform();
+
+        indexPage.clickAddButton();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        wait.until(ExpectedConditions.visibilityOf(layerCard.exitWindow()));
+        assertThat(layerCard.exitWindow().isDisplayed()).isTrue();
+        layerCard.exitWindowSuccess();
+
+        builder.moveToElement(header.moveToShopingCard()).build().perform();
+        wait.until(ExpectedConditions.visibilityOf(header.checkPriceCard()));
+        assertThat(header.checkPriceCard().isDisplayed()).isTrue();
+        header.openShoppingCard();
+
+        String total = String.format("%8.2f", shoppingCard.getTotalPrice());
+        String sum = String.format("%8.2f", shoppingCard.getFirstPrice() + shoppingCard.getSecondPrice() + shoppingCard.getShipping());
+        assertThat(sum).isEqualTo(total);
     }
 
     @Test
