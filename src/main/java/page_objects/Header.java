3437f78fcfc8bb56e5ce64c66d4cf9daa9778cb7
Artek <a.kowalski@ariesrk.pl>
2019-02-15 13:14:24 +0100

    added page_object Header

---
+++ src/main/java/page_objects/Header.java
@@ -0,0 +1,37 @@
+package page_objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Header extends PageObject {
+
+    public Header(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(className = "login")
+    private WebElement loginLink;
+
+    @FindBy(partialLinkText = "Cart")
+    private WebElement shoppingCard;
+
+    @FindBy(id = "button_order_cart")
+    private WebElement pricesCard;
+
+    public void clickLoginLink() {
+        loginLink.click();
+    }
+
+    public WebElement moveToShopingCard() {
+        return shoppingCard;
+    }
+
+    public void openShoppingCard() {
+        shoppingCard.click();
+    }
+
+    public WebElement checkPriceCard() {
+        return pricesCard;
+    }
+}
