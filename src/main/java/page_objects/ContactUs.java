3d345d3e505a3795703dbcdbfa05468b48d2b606
Bartosz Kita <bartosz.kita@gmail.com>
2019-02-21 13:06:18 +0100

    Added new page elements

--- src/main/java/page_objects/ContactUs.java
+++ src/main/java/page_objects/ContactUs.java
@@ -11,16 +11,23 @@ public class ContactUs extends PageObject{
     }
 
     @FindBy(id = "email")
-    private WebElement setEmail;
+    private WebElement setEmail1;
 
     @FindBy(id = "message")
-    private WebElement sendMessage;
+    private WebElement sendMessage1;
+
+    @FindBy(id = "errorMEssage")
+    private WebElement erorrMessage;
 
     public WebElement setEmail() {
-        return setEmail;
+        return setEmail1;
     }
 
     public WebElement sendMessage() {
-        return sendMessage;
+        return sendMessage1;
+    }
+
+    public WebElement checkErrorMessage() {
+        return erorrMessage;
     }
 }
